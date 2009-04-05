826b9b62f08b0bb13400509f884050fb321fe277
Colin Shea <evaryont@gmx.us>
2009-04-05 15:27:39 -0400

    Separated drawing methods into separate file.

---
+++ lib/nccal/draw.rb
@@ -0,0 +1,43 @@
+class NCCal
+	def draw_lines
+		# Draw vertical lines (along the X axis)
+		@wday = 1
+		while (@xinc*@wday) <= Ncurses::COLS
+			Ncurses.mvvline(@starty, (@xinc*@wday), 0, @calheight-2) unless @wday == 7
+			@calwidth = (@xinc*(@wday-1))+1
+			Ncurses.printmid(3, @calwidth, Date::DAYNAMES[@wday-1], @xinc)
+			@wday += 1
+		end
+		@calwidth += @xinc-1
+
+		# Draw horizontal lines (along the Y axis)
+		@week = 1
+		while (@yinc*@week) < @calheight-@yinc
+			Ncurses.mvhline((@yinc*@week)+3, 2, 0, @calwidth)
+			@week += 1
+		end
+	end
+
+	def draw_dates
+		# Draw each date.
+		@dayy = @starty
+		@dayx = (@xinc * @Tm.padding) + 1
+		@current = @Tm.first.dup
+		while (@current.day <= @Tm.last.day and @current.month == @Tm.last.month)
+			if @Tm.selected.day == @current.day
+				Ncurses.attron(Ncurses::A_REVERSE)
+			end
+			Ncurses.mvaddstr(@dayy, @dayx, ("%2s" % @current.day))
+			if @Tm.selected.day == @current.day
+				Ncurses.attroff(Ncurses::A_REVERSE)
+			end
+			@dayx = @xinc * (@current.wday+1) + 1
+			if @current.wday == 6
+				@dayy += @yinc
+				@dayx = 2
+			end
+			@current += 86400
+		end
+
+	end
+end
--- lib/nccal/main.rb
+++ lib/nccal/main.rb
@@ -1,3 +1,5 @@
+require 'lib/nccal/draw.rb'
+
 class NCCal
 	def setup
 		@Tm = NCCal::TimeManager.new
@@ -7,48 +9,6 @@ class NCCal
 		Ncurses.noecho
 		Ncurses.keypad(@window, true)
 		Ncurses.curs_set(0) # Goodbye, cursor
-	end
-
-	def draw_lines
-		# Draw vertical lines (along the X axis)
-		@wday = 1
-		while (@xinc*@wday) <= Ncurses::COLS
-			Ncurses.mvvline(@starty, (@xinc*@wday), 0, @calheight-2) unless @wday == 7
-			@calwidth = (@xinc*(@wday-1))+1
-			Ncurses.printmid(3, @calwidth, Date::DAYNAMES[@wday-1], @xinc)
-			@wday += 1
-		end
-		@calwidth += @xinc-1
-
-		# Draw horizontal lines (along the Y axis)
-		@week = 1
-		while (@yinc*@week) < @calheight-@yinc
-			Ncurses.mvhline((@yinc*@week)+3, 2, 0, @calwidth)
-			@week += 1
-		end
-	end
-
-	def draw_dates
-		# Draw each date.
-		@dayy = @starty
-		@dayx = (@xinc * @Tm.padding) + 1
-		@current = @Tm.first.dup
-		while (@current.day <= @Tm.last.day and @current.month == @Tm.last.month)
-			if @Tm.selected.day == @current.day
-				Ncurses.attron(Ncurses::A_REVERSE)
-			end
-			Ncurses.mvaddstr(@dayy, @dayx, ("%2s" % @current.day))
-			if @Tm.selected.day == @current.day
-				Ncurses.attroff(Ncurses::A_REVERSE)
-			end
-			@dayx = @xinc * (@current.wday+1) + 1
-			if @current.wday == 6
-				@dayy += @yinc
-				@dayx = 2
-			end
-			@current += 86400
-		end
-
 	end
 
 	def loop
